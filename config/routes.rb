a387b67925b30ff141b16233f2e71143bc250000
c-brenn <brennc27@tcd.ie>
2015-09-04 17:47:27 +0100

    added rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   post 'add_members', to: 'members#add_members', as: 'import_members'
   get 'export', to: 'export#index', as: 'export_members'
