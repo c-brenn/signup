b9d6ea0e580b8988cf39b62f44a3992aeaaebae6
Conor Brennan <brennc27@tcd.ie>
2015-09-19 23:37:07 +0100

    Update rack-attack.rb
    Die irwin

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,15 +1,15 @@
 class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+ 
+  Rack::Attack.blacklist('ban login scrapers') do |req|
+   # block clients making a lot of requests for access tokens/login for 1 hour
+     Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 20, :findtime => 1.minute, :bantime => 1.hour) do
+     req.path == '/api/access' || req.path == '/users/sign_in'
+   end
+  end
 
-#  Rack::Attack.blacklist('ban login scrapers') do |req|
-#    # block clients making a lot of requests for access tokens/login for 1 hour
-#    Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 20, :findtime => 1.minute, :bantime => 1.hour) do
-#      req.path == '/api/access' || req.path == '/users/sign_in'
-#    end
-#  end
-#
 #  # throttle requests to import to 2 per second per ip
-#  Rack::Attack.throttle('req/ip', :limit => 2, :period => 1.second) do |req|
-#    req.ip if req.path == '/api/members/import'
-#  end
+  Rack::Attack.throttle('req/ip', :limit => 2, :period => 1.second) do |req|
+    req.ip if req.path == '/api/members/import'
+  end
 end
